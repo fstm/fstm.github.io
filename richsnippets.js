acd91b0995da545215331e5e6b75cc24f4f01a8a
Michael Schaefers <michael.schaefers@scoop-software.de>
2016-06-17 19:30:30 +0200

    use external richsnippets source

---
+++ richsnippets.js
@@ -0,0 +1,28 @@
+<script type="application/ld+json">
+    {
+        "@context": "http://schema.org",
+        "@type": "Localbusiness",
+        "url": "http://fstm.de/",
+        "name": "FSTM GmbH",
+        "address": {
+            "@type": "PostalAddress",
+            "email": "frankschaefers@fstm.de",
+            "addressCountry": "DE",
+            "postalCode": "47445",
+            "streetAddress": "Trakehnenstr. 8b",
+            "addressLocality": "Moers",
+            "faxNumber": "+49 2841 400 695",
+            "telephone": "+49 163 4397337"
+        },
+        "founder": {
+            "@type": "Person",
+            "name": "Frank Schaefers",
+            "givenName": "Frank",
+            "familyName": "Schaefers",
+            "email": "frankschaefers@fstm.de",
+            "gender": "http://schema.org/Male",
+            "jobtitle": "Tischlermeister",
+            "telephone": "+49 163 4397337"
+        }
+    }
+</script>
